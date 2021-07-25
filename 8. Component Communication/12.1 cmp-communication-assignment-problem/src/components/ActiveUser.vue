401fcbe5c964b258c5093aa32f306289dbf7e6e9
PC <khusanov.it.tech@gmail.com>
2021-07-26 00:06:10 +0500

    component

---
+++ 8. Component Communication/12.1 cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li>
+      <h2>{{ name }}</h2>
+      <h3>{{ age }} Years</h3>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script>
