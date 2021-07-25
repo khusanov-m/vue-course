4efbd30d71774fcf72b1ba61005d9851bd086dc7
PC <khusanov.it.tech@gmail.com>
2021-07-26 00:06:12 +0500

    component

---
+++ 8. Component Communication/12.1 cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <form class="form" @submit.prevent="addUser">
+    <input type="text" v-model="enteredName" placeholder="Your name" />
+    <input type="text" v-model="enteredAge" placeholder="Your age" />
+    <button>Add User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-user"],
+
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+
+  methods: {
+    addUser() {
+      this.$emit("add-user", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
