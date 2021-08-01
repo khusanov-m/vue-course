31e201035d29a5a4252555946e388dbace924ae0
PC <khusanov.it.tech@gmail.com>
2021-08-01 23:48:33 +0500

    components

---
+++ 15. Vuex/3. vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <h3>{{ counter }}</h3> 
+  The same with mapGetters below
+  -->
+  <h3>{{ finalCounter }}</h3>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    // counter() {
+    //   return this.$store.getters.finalCounter;
+    // },
+    ...mapGetters('numbers',['finalCounter'])
+  }
+};
+</script>
