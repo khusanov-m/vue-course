4b0dc1a6fa2b04527544b55e2edf75b9d519e48f
PC <khusanov.it.tech@gmail.com>
2021-08-01 23:45:34 +0500

    main vue

---
+++ 14. Animations and Transitions/17. animations-01-starting-setup/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <router-view v-slot="slotProps">
+    <transition name="fade-button" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+button {
+  font: inherit;
+  padding: 0.5rem 2rem;
+  border: 1px solid #810032;
+  border-radius: 30px;
+  background-color: #810032;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #a80b48;
+  border-color: #a80b48;
+}
+
+.block {
+  width: 8rem;
+  height: 8rem;
+  background-color: #290033;
+  margin-bottom: 2rem;
+  /* transition: transform .3s ease-out; */
+}
+
+.container {
+  max-width: 40rem;
+  margin: 2rem auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 2rem;
+  border: 2px solid #ccc;
+  border-radius: 12px;
+}
+
+.animate {
+  /* transform: translateX(-150px);*/
+  animation: slide-slide 0.3s ease-out forwards;
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-button-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
+}
+</style>
