26c45d145cf983b2ec4acc20fc01bca175f8a4ca
PC <khusanov.it.tech@gmail.com>
2021-08-10 13:00:10 +0500

    vue composition API

---
+++ 19. The Composition API - Replacing the Options API/3. composition-01-starting-setup/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="container">
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
+  </section>
+</template>
+
+<script>
+// Composition API
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    // with `ref`
+    const uName = ref('Maximilian');
+    const uAge = ref(31);
+    // with `reactive`
+    // const user = reactive({
+    //   name: 'Maximilian',
+    //   age: 31
+    // });
+
+    function setNewAge() {
+      uName.value = 'Muhammad';
+      uAge.value = 18;
+    }
+
+    // with `ref`
+    // setTimeout(function() {
+    //   // uName.value = 'Muhammad';
+    //   // uAge.value = 18;
+    //   user.name = 'Muhammad';
+    //   user.age = 18
+    // }, 2000);
+
+    // with `ref`
+    return { userName: uName, age: uAge, setAge: setNewAge };
+
+    // with `reactive`
+    // return { user: user, setAge: setNewAge };
+  }
+
+  // Options API
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
+  // methods: {
+  //   setNewAge() {
+  //     this.age = 18;
+  //   }
+  // }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
---
+++ 19. The Composition API - Replacing the Options API/7 composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+
+  <!-- FOR COMPOSITION API -->
+  <!-- REGULAR WAY
+  <h3 v-if="visible">{{ goal }}</h3>
+  <button @click="toggleVisibility">Toggle Goal</button> -->
+
+  <!-- FOR REF 
+  <h3 v-if="visible">{{ courseGoal }}</h3>
+  <button @click="toggleVisibility">Toggle Goal</button> -->
+
+  <!-- FOR REACTIVE
+  <h3 v-if="goal.visible">{{ goal.first }}</h3>
+  <button @click="toggleVisibility">Toggle Goal</button> -->
+
+  <!-- FOR OPTIONS API -->
+  <h3 v-if="visible">{{ goal }}</h3>
+  <button @click="toggleVisibility">Toggle Goal</button>
+
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+// import { ref } from 'vue';
+// import { reactive } from 'vue';
+
+export default {
+  // Options API
+  data() {
+    return {
+      visible: true,
+      goal: 'Develop High Level SPA Projects with help of VueJS'
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.visible = !this.visible;
+    }
+  }
+
+  // Composition API
+  // setup() {
+  // Regular way
+  // const goal = 'Develop High Level SPA Projects with help of VueJS';
+  // let visible = false;
+  // function toggleVisibility() {
+  // visible = !visible;
+  // }
+
+  // return { goal, visible, toggleVisibility: changeVisibility };
+
+  // Ref
+  //   const goal = ref('Develop High Level SPA Projects with help of VueJS');
+  //   const visible = ref(true);
+
+  //   function changeVisibility() {
+  //     visible.value = !visible.value;
+  //   }
+
+  //   return { courseGoal: goal, visible, toggleVisibility: changeVisibility };
+  // }
+
+  // Reactive
+  // const goal = reactive({
+  //   first: 'Develop High Level SPA Projects with help of VueJS',
+  //   visible: true
+  // });
+
+  // function changeVisibility() {
+  //   goal.visible = !goal.visible;
+  // }
+
+  // return { goal: goal, toggleVisibility: changeVisibility };
+  // }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
